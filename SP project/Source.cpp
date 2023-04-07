304c70afad1ba7b6832c16344e6b068579e4551e
Abd El-Rahman Ali <abdel292004@gmail.com>
2023-04-07 17:19:42 +0200

    Add base template

--- SP project/Source.cpp
+++ SP project/Source.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <ctime>
 
 using namespace std;
 
-int main(){}
+struct movie {
+	string name;
+	float price;
+	float overdueFee;
+	float rating; //From 0 to 5
+	bool isRented;
+	int timesRented;
+}movies[100];
+
+struct customer {
+	string name;
+	string rentedMovie;
+	bool isOverdue;
+	tm rentDate;
+	tm returnDate;
+}customers[100];
+
+void addMovie();
+void addCustomer();
+void listMovies();
+void listCustomers();
+void listRentedMovies();
+void rentMovie();
+void returnMovie();
+float calcOverdueFees();
+void listOverdueCustomers();
+void listMostRentedMovies();
+void save();
+void load();
+void listMostRatedMovies();
+
+int main() {}
