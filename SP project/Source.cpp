226e2510ebe311405fbe2d5a11492465c7fcc3c8
Abd El-Rahman Ali <abdel292004@gmail.com>
2023-04-09 17:22:34 +0200

    Add return movie function

--- SP project/Source.cpp
+++ SP project/Source.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 #include <array>
 #include <ctime>
 
 using namespace std;
 
 struct movie {
-	string name;
-	float price;
-	float overdueFee;
-	float rating; //From 0 to 5
-	bool isRented;
-	int timesRented;
+	string name = "\0";
+	float price = 0;		//Per day
+	float overdueFee = 0;	//Per day
+	float rating = 0;		//From 0 to 5
+	bool isRented = false;
+	int timesRented = 0;
 }movies[100];
 
 int numberOfMovies = 0;
 
 struct customer {
-	string name;
-	string rentedMovie;
-	bool isOverdue;
-	tm rentDate;
-	tm returnDate;
+	string name = "\0";
+	string rentedMovie = "\0";
+	bool isOverdue = false;
+	tm rentDate = { 0 };	//To display dates add 1 to months and 1900 to years.
+	tm returnDate = { 0 };	//To display dates add 1 to months and 1900 to years.
 }customers[100];
 
 int numberOfCustomers = 0;
 
 int displayMenu();
+int getCustomerNumber();
 void addMovie();
 void addCustomer();
 void listMovies();
@@ -35,12 +37,13 @@ void listCustomers();
 void listRentedMovies();
 void rentMovie();
 void returnMovie();
-float calcOverdueFees();
 void listOverdueCustomers();
 void listMostRentedMovies();
 void save();
 void load();
 void listMostRatedMovies();
+void resetCustomer(int customerIndex, int& movieIndex);
+float getValidNumber();
 
 int main() {
 	char answer = '\0';
@@ -122,4 +125,77 @@ int displayMenu() {
 	cin >> option;
 
 	return option;
+}
+
+void returnMovie() {
+	int customerIndex = 0;
+	int movieIndex = 0;
+	int daysdiff = 0;
+	int overdiff = 0;
+
+	listCustomers(); //change to list renting customers
+
+	customerIndex = getCustomerNumber();
+
+	overdiff = difftime(time(NULL), mktime(&customers[customerIndex].returnDate)) / 86400;
+	daysdiff = ceil(difftime(mktime(&customers[customerIndex].returnDate), mktime(&customers[customerIndex].rentDate)) / 86400);
+
+	resetCustomer(customerIndex, movieIndex);
+
+	if (overdiff < 0) {
+		overdiff = 0;
+	}
+
+	cout << "Please pay: " << daysdiff * movies[movieIndex].price + overdiff * movies[movieIndex].overdueFee << endl;
+}
+
+int getCustomerNumber() {
+	int index = 0;
+
+	while (true) {
+		cout << "Enter customer number: ";
+
+		index = getValidNumber();
+
+		if (index > 0 && index <= numberOfCustomers) {
+			--index;
+
+			break;
+		}
+
+		cout << "Error:Customer does not exist." << endl;
+	}
+
+	return index;
+}
+
+float getValidNumber() {
+	float number = 0;
+
+	cin >> number;
+
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		cout << "Enter numbers only: ";
+		cin >> number;
+	}
+
+	return number;
+}
+
+void resetCustomer(int customerIndex, int& movieIndex) {
+	for (movieIndex; movieIndex < numberOfMovies; ++movieIndex) {
+		if (movies[movieIndex].name == customers[customerIndex].rentedMovie) {
+			movies[movieIndex].isRented = false;
+
+			break;
+		}
+	}
+
+	customers[customerIndex].rentedMovie = "\0";
+	customers[customerIndex].isOverdue = false;
+	customers[customerIndex].rentDate = { 0 };
+	customers[customerIndex].returnDate = { 0 };
 }
